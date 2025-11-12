8b164bd97980291fb9069b4e85e3fc4dc7434d81
Guillermovillacuratorres <127245210+Guillermovillacuratorres@users.noreply.github.com>
2025-11-12 18:25:54 -0300

    Clase 12-11-2025

---
+++ CRUD/src/crud/CRUD.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crud;
+
+import views.Listarpersonas;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class CRUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Listarpersonas l = new Listarpersonas();
+        l.setVisible(true);
+    }
+    
+}
